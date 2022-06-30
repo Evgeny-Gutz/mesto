abcdd5b112b86d309bf7cbc14f41abca651e1cde
Evgeny Gutz <evgeny.gutz@yandex.ru>
2022-07-01 01:17:35 +0300

    Исправил все замечания

--- scripts/index.js
+++ scripts/index.js
@@ -1,47 +1,40 @@
-let popupElem = document.querySelector('.popup');
-let formElement = popupElem.querySelector('.popup__form');
-let formCross = document.querySelector('.popup__cross');
+let popupElem = document.querySelector('.popup'),
+    inputName = popupElem.querySelector('.popup__input_type_name'),
+    inputProfession = popupElem.querySelector('.popup__input_type_job'),
+    popupForm = popupElem.querySelector('.popup__form'),
+    formSubmit = popupElem.querySelector('.popup__submite'),
+    formCross = popupElem.querySelector('.popup__cross');
 
-let profileName = document.querySelector('.profile__name');
-let profileProfession = document.querySelector('.profile__profession');
-let profileEditButton = document.querySelector('.profile__edit-button');
+let profileName = document.querySelector('.profile__name'),
+    profileProfession = document.querySelector('.profile__profession'),
+    editButton = document.querySelector('.profile__edit-button');
 
-let inputName = formElement.querySelector(".popup__input[name='name']");
-let inputProfession = formElement.querySelector(".popup__input[name='profession']");
+editButton.addEventListener('click', openPopup);
 
-// Перед работой торопился, скрипт переделать не успел, проверте код без скрипта.... Спасибо.
+formCross.addEventListener('click', visibilityChange);
 
-formCross.addEventListener('click', closePopup());
-popupElem.addEventListener('click', closePopup());
-console.log(inputName);
+formSubmit.addEventListener('click', visibilityChange)
 
-popupElem.addEventListener('keydown', function(event) {
-      if (event.code == 'Enter') {
-        inputName.value = profileName.textContent;
-        inputProfession.value = profileProfession.textContent;
-        popupElem.style.display = 'none';
-      }
-    });
+popupForm.addEventListener('submit', formSubmitHandler);
 
-profileEditButton.addEventListener('click', openPopup);
-formElement.addEventListener('submit', formSubmitHandler);
-
-function closePopup () {
-    inputName.value = profileName.textContent;
-    inputProfession.value = profileProfession.textContent;
-    popupElem.style.display = 'none';
-}
+popupElem.addEventListener('click', function (evt) {
+    if (evt.target.classList.contains('popup__opened')) {
+        visibilityChange();
+    }
+})
 
 function openPopup () {
-    popupElem.style.display = 'flex';
     inputName.value = profileName.textContent;
     inputProfession.value = profileProfession.textContent;
+    visibilityChange ();
 }
 
 function formSubmitHandler (evt) {
     evt.preventDefault();
     profileName.textContent = inputName.value;
     profileProfession.textContent = inputProfession.value;
-    popupElem.style.display = 'none';
 }
- 
+
+function visibilityChange () {
+    popupElem.classList.toggle('popup__opened');
+}
