f1d0ee0c2ff2bb390513516842c32ac9075c5f71
Evgeny Gutz <evgeny.gutz@yandex.ru>
2022-11-09 21:06:44 +0300

    refactor: Настроить Wtbpack. Done 90%

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin'); 
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin'); 
 
 module.exports = {
     entry: {
@@ -24,6 +25,24 @@ module.exports = {
                 test: /\.js$/,
                 use: 'babel-loader',
                 exclude: '/node_modules/'
+            },
+            {
+                test: /\.css$/,
+                use: [MiniCssExtractPlugin.loader, {loader: 'css-loader'}]
+            },
+            {
+                test: /\.(png|svg|jpg|gif|woff(2)?|eot|ttf|otf)$/,
+                type: 'asset/resource',
+                generator: {
+                    filename: 'images/[name].[hash][ext]'
+                }
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/,
+                type: 'asset/resource',
+                generator: {
+                    filename: 'fonts/[name].[hash][ext]'
+                }
             }
         ]
     },
@@ -32,5 +51,6 @@ module.exports = {
             template: './src/index.html'
         }),
         new CleanWebpackPlugin(),
+        new MiniCssExtractPlugin()
     ]
 }   
